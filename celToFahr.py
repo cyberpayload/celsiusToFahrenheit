dab4b9b83cf6f36de99380eb58ec8af3960898e5
cyberpayload <will@cyberpayload.com>
2020-11-27 10:08:26 -0600

    added user input and store in variable for current temperature

--- celToFahr.py
+++ celToFahr.py
@@ -1,15 +1,15 @@
 ########## CONVERT CELSIUS TO FAHRENHEIT and Vice Versa ##########
 
 # create variables for celsius equation
-
 # from Fahrenheit to Celsius
 
-# from Celsius to Fahrenheith
+# from Celsius to Fahrenheit
 
 # display menu to user
-
+currentTemp = input('ENTER YOUR CURRENT TEMPERATURE: ')
+print(currentTemp)
 # store user answer in variable
 
 # perform equation
 
-# dispaly answer
+# display answer
