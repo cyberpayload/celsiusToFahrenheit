0ee169b83d2617ea9498231cfb32af5cf7691ce8
cyberpayload <will@cyberpayload.com>
2020-11-27 11:35:10 -0600

    added print statements with equations

--- celToFahr.py
+++ celToFahr.py
@@ -6,7 +6,7 @@
 currentTemp = input('ENTER YOUR CURRENT TEMPERATURE: ')
 
 # ask user if their current temp is in Celsius or Fahrenheit
-geo = input('CELCIUS OR FAHRENHEIT?: ( C OR F )')
+geo = input('CELCIUS OR FAHRENHEIT? ( C OR F ): ')
 
 
 # perform equation
@@ -16,11 +16,23 @@ else:
     if geo == "f" or geo == "F":
         currentTempFahr = currentTemp
 
-# create variables for celsius equation
+# create variables for Celsius equation
+
+
 # from Fahrenheit to Celsius
+currentTempFromFahr = (float(currentTemp) * 1.8 + 32)
 
 # from Celsius to Fahrenheit
-
+currentTempFromCel = (float(currentTemp) / 1.8 - 32)
 
 
 # display answer
+if geo == "c" or geo == "C":
+    print("Your current temperautre is " + "\n" 
+    + currentTemp + " Celsius" + "\n"
+    + str(currentTempFromFahr) + " Fahrenheit")
+else:
+    if geo == "f" or geo == "F":
+        print("Your current temperautre is " + "\n" 
+        + currentTemp + " Fahrenheit" + "\n"
+        + str(currentTempFromCel) + " Celsius")
