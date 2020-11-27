bfaf8b07bdb945c2001fe7eb357b9747ad2b4ffb
cyberpayload <will@cyberpayload.com>
2020-11-27 11:39:13 -0600

    added a definite while loop, still no error handling

--- celToFahr.py
+++ celToFahr.py
@@ -1,38 +1,38 @@
 ########## CONVERT CELSIUS TO FAHRENHEIT and Vice Versa ##########
 
 
-
+while True:
 # ask user for current temp and store in a variable
-currentTemp = input('ENTER YOUR CURRENT TEMPERATURE: ')
+    currentTemp = input('ENTER YOUR CURRENT TEMPERATURE: ')
 
 # ask user if their current temp is in Celsius or Fahrenheit
-geo = input('CELCIUS OR FAHRENHEIT? ( C OR F ): ')
+    geo = input('CELCIUS OR FAHRENHEIT? ( C OR F ): ')
 
 
 # perform equation
-if geo == "c" or geo == "C":
-    currentTempCel = currentTemp
-else:
-    if geo == "f" or geo == "F":
-        currentTempFahr = currentTemp
+    if geo == "c" or geo == "C":
+        currentTempCel = currentTemp
+    else:
+        if geo == "f" or geo == "F":
+            currentTempFahr = currentTemp
 
 # create variables for Celsius equation
 
 
 # from Fahrenheit to Celsius
-currentTempFromFahr = (float(currentTemp) * 1.8 + 32)
+    currentTempFromFahr = (float(currentTemp) * 1.8 + 32)
 
 # from Celsius to Fahrenheit
-currentTempFromCel = (float(currentTemp) / 1.8 - 32)
+    currentTempFromCel = (float(currentTemp) / 1.8 - 32)
 
 
 # display answer
-if geo == "c" or geo == "C":
-    print("Your current temperautre is " + "\n" 
-    + currentTemp + " Celsius" + "\n"
-    + str(currentTempFromFahr) + " Fahrenheit")
-else:
-    if geo == "f" or geo == "F":
+    if geo == "c" or geo == "C":
         print("Your current temperautre is " + "\n" 
-        + currentTemp + " Fahrenheit" + "\n"
-        + str(currentTempFromCel) + " Celsius")
+        + currentTemp + " Celsius" + "\n"
+        + str(currentTempFromFahr) + " Fahrenheit")
+    else:
+        if geo == "f" or geo == "F":
+            print("Your current temperautre is " + "\n" 
+            + currentTemp + " Fahrenheit" + "\n"
+            + str(currentTempFromCel) + " Celsius")
