377a29fa82db5d93e48719d591abe2f9e5a4f25b
cyberpayload <will@cyberpayload.com>
2020-11-27 10:25:03 -0600

    added if statement to capture user current temp and if Cel or Fahr, also moved some commentting downward

--- celToFahr.py
+++ celToFahr.py
@@ -1,15 +1,26 @@
 ########## CONVERT CELSIUS TO FAHRENHEIT and Vice Versa ##########
 
+
+
+# ask user for current temp and store in a variable
+currentTemp = input('ENTER YOUR CURRENT TEMPERATURE: ')
+
+# ask user if their current temp is in Celsius or Fahrenheit
+geo = input('CELCIUS OR FAHRENHEIT?: ( C OR F )')
+
+
+# perform equation
+if geo == "c" or geo == "C":
+    currentTempCel = currentTemp
+else:
+    if geo == "f" or geo == "F":
+        currentTempFahr = currentTemp
+
 # create variables for celsius equation
 # from Fahrenheit to Celsius
 
 # from Celsius to Fahrenheit
 
-# display menu to user
-currentTemp = input('ENTER YOUR CURRENT TEMPERATURE: ')
-print(currentTemp)
-# store user answer in variable
 
-# perform equation
 
 # display answer
