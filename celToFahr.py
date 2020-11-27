0f6862f55843343a9bd2833106446af86beab684
cyberpayload <will@cyberpayload.com>
2020-11-27 09:55:27 -0600

    added commenting structure rough draft

--- celToFahr.py
+++ celToFahr.py
@@ -1,2 +1,15 @@
 ########## CONVERT CELSIUS TO FAHRENHEIT ##########
 
+# create variables for celsius equation
+
+# from Fahrenheit to Celsius
+
+# from Celsius to Fahrenheith
+
+# display menu to user
+
+# store user answer in variable
+
+# perform equation
+
+# dispaly answer
